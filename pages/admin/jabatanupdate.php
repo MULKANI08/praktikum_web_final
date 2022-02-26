82d3f5318fe8179a79b093fd66c080af001aa954
MULKANI08 <kanie.ahmad01@gmail.com>
2022-02-26 20:33:25 +0800

    Praktikum Web Final

---
+++ pages/admin/jabatanupdate.php
@@ -0,0 +1,109 @@
+<?php
+if (isset($_GET['id'])){
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $id = $_GET['id'];
+    $findSQL = "SELECT * FROM jabatan WHERE id = ?";
+    $stmt = $db->prepare($findSQL);
+    $stmt->bindParam(1, $_GET ['id']);
+    $stmt->execute();
+    $row = $stmt->fetch();
+    if (isset($row['id'])) {
+        if (isset($_POST['button_update'])){
+
+            $database = new Database();
+            $db = $database->getConnection();
+        
+            $validateSQL = "SELECT * FROM jabatan WHERE nama_jabatan = ? AND id != ?";
+            $stmt = $db->prepare($validateSQL);
+            $stmt->bindParam(1, $_POST ['nama_jabatan']);
+            $stmt->bindParam(2, $_POST ['id']);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0){
+        ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+                    Nama Jabatan Sama Sudah Ada
+                </div>
+        <?php
+            } else {
+                $updateSQL = "UPDATE jabatan SET nama_jabatan = ? WHERE id = ?";
+                $stmt = $db->prepare($updateSQL);
+                $stmt->bindParam(1, $_POST ['nama_jabatan']);
+                $stmt->bindParam(2, $_POST ['id']);
+                if ($stmt->execute()) {
+                    $_SESSION['hasil'] = true;
+                    $_SESSION['pesan'] = "Berhasil Ubah Data";
+                    // echo "Simpan Berhasil";
+                } else {
+                    $_SESSION['hasil'] = false;
+                    $_SESSION['pesan'] = "Gagal Ubah Data";
+                    // echo "Simpan Gagal";
+                }
+                echo "<meta http-equiv='refresh' content='0;url=?page=jabatanread'>";
+            }
+        }
+        ?>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb2">
+                    <div class="col-sm-6">
+                        <h1>Ubah Data Jabatan</h1>
+                    </div>                
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                            <li class="breadcrumb-item"><a href="?page=jabatanread">Jabatan</a></li>
+                            <li class="breadcrumb-item active">Ubah Data</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Ubah Jabatan</h3>
+                </div>
+                <div class="card-body">
+                    <form method="POST">
+                        <div class=form-grup>
+                            <label for="nama_jabatan">Nama Jabatan</label>
+                            <input type="hidden" class="form-control" name="id" value="<?php echo $row['id'] ?>">
+                            <input type="text" class="form-control" name="nama_jabatan" value="<?php echo $row['nama_jabatan'] ?>">
+                        </div>
+                        <div class=form-grup>
+                            <label for="nama_jabatan">Gaji Pokok</label>
+                            <input type="hidden" class="form-control" name="id" value="<?php echo $row['id'] ?>">
+                            <input type="number" class="form-control" name="gapok_jabatan" value="<?php echo $row['gapok_jabatan'] ?>">
+                        </div>
+                        <div class=form-grup>
+                            <label for="tunjangan_jabatan">Tunjangan</label>
+                            <input type="hidden" class="form-control" name="id" value="<?php echo $row['id'] ?>">
+                            <input type="number" class="form-control" name="tunjangan_jabatan" value="<?php echo $row['tunjangan_jabatan'] ?>">
+                        </div>
+                        <div class=form-grup>
+                            <label for="uang_makan_perhari">Uang Makan Perhari</label>
+                            <input type="hidden" class="form-control" name="id" value="<?php echo $row['id'] ?>">
+                            <input type="number" class="form-control" name="uang_makan_perhari" value="<?php echo $row['uang_makan_perhari'] ?>">
+                        </div>
+                        <a href="?page=jabatanread" class="btn btn-danger btn-sm float-right">
+                            <i class="fa fa-times"></i> Batal
+                        </a>
+                        <button type="submit" name="button_update" class="btn btn-success btn-sm float-right">
+                            <i class="fa fa-save"></i> Simpan
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </section>
+<?php
+    } else {
+        echo "<meta http-equiv='refresh' content='0;url=?page=jabatanread'>";
+    }
+} else {
+    echo "<meta http-equiv='refresh' content='0;url=?page=jabatanread'>";
+}
---
+++ pages/admin/karyawancreate.php
@@ -0,0 +1,157 @@
+<?php
+if (isset($_POST['button_create'])){
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $validateSQL = "SELECT * FROM karyawan WHERE nik = ?";
+    $stmt = $db->prepare($validateSQL);
+    $stmt->bindParam(1, $_POST ['nik']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0){
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+            NIK Sama Sudah Ada
+        </div>
+<?php
+    } else {
+    $validateSQL = "SELECT * FROM pengguna WHERE username = ?";
+    $stmt = $db->prepare($validateSQL);
+    $stmt->bindParam(1, $_POST ['username']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0){
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+            Username Sama Sudah Ada
+        </div>
+<?php    
+    } else{
+    if ($_POST['password'] != $_POST['password_ulangi']){
+        ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+                    Password Tidak Sama
+                </div>
+<?php
+    } else {
+        $md5Password = md5($_POST['password']);
+
+        $insertSQL = "INSERT INTO pengguna VALUES (NULL, ?, ?, ?, NULL)";
+        $stmt = $db->prepare($insertSQL);
+        $stmt->bindParam(1, $_POST ['username']);
+        $stmt->bindParam(2, $md5Password);
+        $stmt->bindParam(3, $_POST ['peran']);
+
+        if ($stmt->execute()) {
+
+            $pengguna_id = $db->lastInsertId();
+
+            $insertKaryawanSql = "INSERT INTO karyawan VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+            $stmtKaryawan = $db->prepare($insertKaryawanSql);
+            $stmtKaryawan->bindParam(1, $_POST['nik']);
+            $stmtKaryawan->bindParam(2, $_POST['nama_lengkap']);
+            $stmtKaryawan->bindParam(3, $_POST['handphone']);
+            $stmtKaryawan->bindParam(4, $_POST['email']);
+            $stmtKaryawan->bindParam(5, $_POST['tanggal_masuk']);
+            $stmtKaryawan->bindParam(6, $pengguna_id);
+
+            if ($stmtKaryawan->execute()){
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil Simpan Data";
+            // echo "Simpan Berhasil";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Gagal Simpan Data";
+            // echo "Simpan Gagal";
+        }
+        } else {
+        $_SESSION['hasil'] = false;
+        $_SESSION['pesan'] = "Gagal Simpan Data";    
+        }
+        echo "<meta http-equiv='refresh' content='0;url=?page=karyawanread'>";
+    }
+    }
+    }
+}
+?>
+
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb2">
+            <div class="col-sm-6">
+                <h1>Tambah Data Karyawan</h1>
+            </div>                
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                    <li class="breadcrumb-item"><a href="?page=karyawanread">Karyawan</a></li>
+                    <li class="breadcrumb-item active">Tambah Data</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Karyawan</h3>
+        </div>
+        <div class="card-body">
+            <form method="POST">
+                <div class=form-grup>
+                    <label for="nik">Nomor Induk Karyawan (NIK)</label>
+                    <input type="text" class="form-control" name="nik">
+                </div>
+                <div class=form-grup>
+                    <label for="nama_lengkap">Nama Lengkap</label>
+                    <input type="text" class="form-control" name="nama_lengkap">
+                </div>
+                <div class=form-grup>
+                    <label for="handphone">Handphone</label>
+                    <input type="text" class="form-control" name="handphone">
+                </div>
+                <div class=form-grup>
+                    <label for="email"> Email</label>
+                    <input type="email" class="form-control" name="email">
+                </div>
+                <div class=form-grup>
+                    <label for="tanggal_masuk">Tanggal Masuk</label>
+                    <input type="date" class="form-control" name="tanggal_masuk">
+                </div>
+                <div class=form-grup>
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" name="username">
+                </div>
+                <div class=form-grup>
+                    <label for="PASSWORD">Password</label>
+                    <input type="password" class="form-control" name="PASSWORD">
+                </div>
+                <div class=form-grup>
+                    <label for="PASSWORD">Password (Ulangi)</label>
+                    <input type="password" class="form-control" name="PASSWORD">
+                </div>
+                <div class=form-grup>
+                    <label for="peran">Peran</label>
+                    <select class="form-control" name="peran">
+                        <option value="">-- Pilih Peran --</option>
+                        <option value="ADMIN">ADMIN</option>
+                        <option value="USER">USER</option>
+                    </select>                    
+                </div>
+                <a href="?page=karyawanread" class="btn btn-danger btn-sm float-right">
+                    <i class="fa fa-times"></i> Batal
+                </a>
+                <button type="submit" name="button_create" class="btn btn-success btn-sm float-right">
+                    <i class="fa fa-save"></i> Simpan
+                </button>
+            </form>
+        </div>
+    </div>
+</section>
+
+<?php include_once "partials/scripts.php" ?>
